d1dc6e9c0f8428b5fe1b510a8043f016ada8f8d1
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2021-04-22 12:12:31 +0200

    Ajout du board

---
+++ game.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board,piece):
+    #Horizontal voir si il y a un gagnant
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2]and board[r][c+3]:
+                return True
+
+
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    if turn == 0:
+        col = int(input("Player 1 make your selection"))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+
+    else:
+        col = int(input("Player 2 make your selection"))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+    print_board(board)
+    turn += 1
+    turn = turn % 2
