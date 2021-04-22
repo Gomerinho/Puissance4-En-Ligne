26ef94d8fe9b2a12f6726406ec871d4a204aa235
Raphaël Oester <raphael.oester@ynov.com>
2021-04-22 17:54:09 +0200

    client complete

--- client.py
+++ client.py
@@ -5,10 +5,23 @@ HEADER = 64
 PORT = 40000
 FORMAT = 'utf-8'
 DECONNEXION = "!DECONNEXION"
-SERVER = "192.168.1.2"
+SERVER = "vps-aef73ebf.vps.ovh.net"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 client.connect(ADDR)
 
+def envoyer(msg):
+    message = msg.encode(FORMAT)
+    message_longueur = str(len(message)).encode(FORMAT)
+    message_longueur += b' '*(HEADER-len(message_longueur))
+
+    client.send(message_longueur)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+def communication():
+    while (True):
+        envoyer(input())
+
+communication()
